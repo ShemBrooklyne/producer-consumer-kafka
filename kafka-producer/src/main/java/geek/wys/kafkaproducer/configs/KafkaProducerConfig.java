c9544873a45147556ee899a721c8993115d3393a
Shem Brooklyne Mwangi <shem.mwangi@cellulant.io>
2024-04-03 20:39:41 +0300

    kafka producer template configuration and docker-compose setup

---
+++ kafka-producer/src/main/java/geek/wys/kafkaproducer/configs/KafkaProducerConfig.java
@@ -0,0 +1,35 @@
+package geek.wys.kafkaproducer.configs;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author shem.mwangi
+ * @since 03/04/2024
+ **/
+@Configuration
+public class KafkaProducerConfig {
+
+    @Bean
+    public ProducerFactory<String, Object> employeeProducerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class.getName());
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> employeeKafkaTemplate() {
+        return new KafkaTemplate<>(employeeProducerFactory());
+    }
+}
