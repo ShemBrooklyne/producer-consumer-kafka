080987c8a5296d7595ed27fea7e3c22f5c82541e
Shem Brooklyne Mwangi <shem.mwangi@cellulant.io>
2024-04-03 22:08:52 +0300

    initial commit: kafka employee consumer

---
+++ kafka-consumer/src/main/java/geek/wys/kafkaconsumer/KafkaConsumerApplication.java
@@ -0,0 +1,17 @@
+package geek.wys.kafkaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shem.mwangi
+ * @since 03/04/2024
+ **/
+@SpringBootApplication
+public class KafkaConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaConsumerApplication.class, args);
+    }
+
+}
