58824444d70c720e28d15b1a060b6ce18db28e7c
Shem Brooklyne Mwangi <shem.mwangi@cellulant.io>
2024-04-03 14:47:41 +0300

    initial commit

---
+++ kafka-producer/src/main/java/geek/wys/kafkaproducer/KafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package geek.wys.kafkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaProducerApplication.class, args);
+    }
+
+}
---
+++ kafka-producer/src/test/java/geek/wys/kafkaproducer/KafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package geek.wys.kafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
