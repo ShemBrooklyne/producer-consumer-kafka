1de08622689237d95f0c2119fe336b62812112aa
Shem Brooklyne Mwangi <shem.mwangi@cellulant.io>
2024-04-03 14:53:51 +0300

    dependency cleanup

--- kafka-producer/src/main/java/geek/wys/kafkaproducer/KafkaProducerApplication.java
+++ kafka-producer/src/main/java/geek/wys/kafkaproducer/KafkaProducerApplication.java
@@ -3,6 +3,10 @@ package geek.wys.kafkaproducer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author shem.mwangi
+ * @since 03/04/2024
+ **/
 @SpringBootApplication
 public class KafkaProducerApplication {
 
